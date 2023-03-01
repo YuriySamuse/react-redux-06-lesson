9e24fdc9f7af5b087e13da357c2e272ab055d277
YuriySamusew <6177385@gmail.com>
2023-03-01 20:06:32 +0200

    Встановлення, Стор, Редах девтулс, Підписка на стор.

---
+++ src/components/StatusFilter/StatusFilter.js
@@ -0,0 +1,19 @@
+// Імпортуємо хук
+import { useSelector } from 'react-redux';
+import { Button } from 'components/Button/Button';
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from 'redux/constants';
+import { getStatusFilter } from 'redux/selectors';
+import css from './StatusFilter.module.css';
+
+export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(getStatusFilter);
+  return (
+    <div className={css.wrapper}>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
+    </div>
+  );
+};
